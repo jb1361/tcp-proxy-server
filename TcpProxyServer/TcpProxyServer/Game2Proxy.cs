a6a79a1b7289299a4e9dc9ec3f572db45784ee38
jb1361 <justinbutler4@hotmail.com>
2018-12-18 18:46:12 -0500

    Create game2proxy class

---
+++ TcpProxyServer/TcpProxyServer/Game2Proxy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpProxyServer
+{
+    class Game2Proxy
+    {
+        public string host;
+        public int port;
+        public Proxy2Server Server;
+        private TcpClient client;
+        private TcpListener listener;
+        public Game2Proxy(string host, int port)
+        {
+            host = this.host;
+            port = this.port;
+            client = new TcpClient();
+            StartListener();
+        }   
+        public void StartListener()
+        {
+            IPAddress ip = IPAddress.Parse(host);
+            listener = new TcpListener(ip, port);
+            listener.Start();
+            Byte[] bytes = new Byte[4096];
+            String data = null;
+            // Enter the listening loop.
+            while (true)
+            {
+                client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                data = null;
+                int i;
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+                }
+                Console.WriteLine("Server: " + data);               
+            }
+        }
+        public void SendData2Game(Byte[] bytes)
+        {
+
+        }
+    }
+}
---
+++ TcpProxyServer/TcpProxyServer/Proxy2Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpProxyServer
+{
+    /// <summary>
+    /// Opens a tcplistener on all ip address ranges and port 4000. It listens for any tcp data being sent through then writes it to the console.
+    /// </summary>
+    class Proxy2Server
+    {
+        Game2Proxy game = null;
+        public Proxy2Server(IPAddress ip, int port)
+        {  
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+            Byte[] bytes = new Byte[4096];
+            String data = null;
+            // Enter the listening loop.
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                data = null;
+                int i;
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+                }
+                Console.WriteLine("Server: " + data);               
+            }
+        }
+
+        public void ForwardData()
+        {
+
+        }
+    }
+}
